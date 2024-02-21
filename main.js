6260e4a5b9da8301f6b8f0c21901409c61efc386
Anand raja <meetanandns@gmail.com>
2024-02-21 08:33:30 +0000

    logic added

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import './src/styles/app.css';
 
-import './src/scripts/app';
-import './src/scripts/vendor';
-
 import htmlStrHome from './src/views/main.html?raw';
+
+import { add, subtract, multiply, divide } from './src/scripts/app';
+
 
 document.querySelector('#app').innerHTML = htmlStrHome;
 
+const addBtn = document.getElementById('btn-add');
+const subtractBtn = document.getElementById('btn-subtract');
+const multiplyBtn = document.getElementById('btn-multiply');
+const divideBtn = document.getElementById('btn-divide');
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
